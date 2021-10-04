dd74624fbe6c7bec5fff8b6854345978c74afbff
kwl3434 <kwl3434@gmail.com>
2021-10-04 21:39:16 +0900

    feature:루틴 추가 선택시 달력 기능 추가, 삭제 추가 루틴 추가 선택시 달력 기능 추가 루틴 삭제시 삭제 알림창 추가
    메뉴, 버튼 xml 작성 #11
    
    개발자 테스트
    xml로 no test

--- app/src/main/java/com/example/baseapp/MainActivity.java
+++ app/src/main/java/com/example/baseapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.baseapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -15,8 +16,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner SpinnerRoutine1,SpinnerRoutine2;
+    Spinner SpinnerRoutine1, SpinnerRoutine2;
     com.google.android.material.floatingactionbutton.FloatingActionButton ButtonRoutine1;
+    View dialogView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,28 +32,17 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(ButtonRoutine1);
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-
-
-        return true;
-    }
-*/
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater mInflater = getMenuInflater();
-        if (v == SpinnerRoutine1){
+        if (v == SpinnerRoutine1) {
             menu.setHeaderTitle("루틴 편집/삭제");
             mInflater.inflate(R.menu.menu1, menu);
-        }
-        else if(v == SpinnerRoutine2){
+        } else if (v == SpinnerRoutine2) {
             menu.setHeaderTitle("루틴 편집/삭제");
             mInflater.inflate(R.menu.menu1, menu);
-        }
-        else if(v == ButtonRoutine1){
+        } else if (v == ButtonRoutine1) {
             menu.setHeaderTitle("루틴 추가");
             mInflater.inflate(R.menu.menu2, menu);
         }
@@ -58,10 +50,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            default:
+            case R.id.itemdelete:
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("삭제");
+                dlg.setMessage("삭제하시겠습니까?");
+                dlg.setPositiveButton("예", null);
+                dlg.setNegativeButton("아니요", null);
+                dlg.show();
                 return true;
+            case R.id.addRoutine:
+                dialogView = (View) View.inflate(MainActivity.this, R.layout.calendar, null);
+                AlertDialog.Builder dlg2 = new AlertDialog.Builder(MainActivity.this);
+                dlg2.setTitle("루틴 추가");
+                dlg2.setView(dialogView);
+                dlg2.setNegativeButton("닫기", null);
+                dlg2.show();
+                return true;
+            default:
+                return false;
         }
     }
 }
